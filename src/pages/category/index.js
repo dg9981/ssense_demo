6869bafd7a598c3c6de66e077ace799e9f87cc79
dg9981 <dg9981v@gmail.com>
2020-06-28 02:31:21 +0800

    示意 男/女 分类页

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { useActions, useValues } from "kea";
+import GlobalRouter from "../../common/global_router";
+import styled from "styled-components";
+
+const CategoryDiv = styled.div`
+	width: 100%;
+	height: 100%;
+	font-size: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	span {
+		margin-top: -10%;
+	}
+`;
 
 export default function(){
-    return <div>category</div>
+	let { curScene, curUrlParams } = useValues(GlobalRouter);
+	return (
+		<CategoryDiv>
+			<span>{ curScene + " : " + curUrlParams.lang + " / " + curUrlParams.sex }</span>
+		</CategoryDiv>
+	);
 };
